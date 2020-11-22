5820f525a0cb161a7820966ac4a7e34c33baee78
changbinbin <binbinchang@126.com>
2020-11-22 14:56:47 +0800

    origin

---
+++ build/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/main.cpp.obj"
+  "gtest.exe"
+  "gtest.exe.manifest"
+  "gtest.pdb"
+  "libgtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "gtest project" << endl;
+    return 0;
+}
