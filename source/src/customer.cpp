d46c0df799e3c8f17e893851daae889d684a9e05
changbinbin <binbinchang@126.com>
2020-12-19 00:28:23 +0800

    refactory step2: rename method and variable name

--- source/src/customer.cpp
+++ source/src/customer.cpp
@@ -30,29 +30,29 @@ string Customer::GetName()
     return this->name;
 }
 
-static double GetEachAmount(Rental &eachRental)
+static double GetAmountForRental(Rental &aRental)
 {
-    double amount = 0;
-    switch (eachRental.GetMovie().GetPriceCode()) {
+    double amountResult = 0;
+    switch (aRental.GetMovie().GetPriceCode()) {
     case Movie::regular:
-        amount += 2;
-        if (eachRental.GetDaysRented() > 2) {
-            amount += (eachRental.GetDaysRented() - 2) * 1.5;
+        amountResult += 2;
+        if (aRental.GetDaysRented() > 2) {
+            amountResult += (aRental.GetDaysRented() - 2) * 1.5;
         }
         break;
     case Movie::newRelease:
-        amount += eachRental.GetDaysRented() * 3;
+        amountResult += aRental.GetDaysRented() * 3;
         break;
     case Movie::childrens:
-        amount += 1.5;
-        if (eachRental.GetDaysRented() > 3) {
-            amount += (eachRental.GetDaysRented() - 3) * 1.5;
+        amountResult += 1.5;
+        if (aRental.GetDaysRented() > 3) {
+            amountResult += (aRental.GetDaysRented() - 3) * 1.5;
         }
         break;
     default:
         break;
     }
-    return amount;
+    return amountResult;
 }
 
 string Customer::Statement()
@@ -62,7 +62,7 @@ string Customer::Statement()
     string result = "Rental Record for " + this->GetName() + "\n";
     vector<Rental>::iterator it;
     for (it = this->rentals.begin(); it != this->rentals.end(); it++) {
-        double amount = GetEachAmount(*it);
+        double amount = GetAmountForRental(*it);
         frequentRenterPoints++;
         if ((it->GetMovie().GetPriceCode() == Movie::newRelease) && it->GetDaysRented() > 1) {
             frequentRenterPoints++;
